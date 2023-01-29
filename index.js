cf158e9697ad77825b09135a9d06c610f6f5c47f
Zeger Knops <zeger@zeger.nl>
2023-01-29 16:37:58 +0000

    - removed webserver code - cleanup packages

--- index.js
+++ index.js
@@ -9,21 +9,11 @@
  */
 'use strict';
 
-const fs = require('fs');
-const os = require('os');
-const path = require('path');
-
-
-const express = require('express');
-const morgan = require('morgan');
-const request = require('request');
 const winston = require('winston');
 const mqtt = require('mqtt');
 
-
 const {
     Converter,
-    DLxJsonConverter,
     FileSystemRecorder,
     HeaderSet,
     HeaderSetConsolidator,
@@ -31,18 +21,12 @@ const {
     SerialConnection,
     Specification,
     TcpConnection,
-    TextConverter,
-    VBusRecordingConverter,
 } = require('resol-vbus');
 
 
 const config = require('./config');
 
-
-
 const specification = Specification.getDefaultSpecification();
-
-
 
 const logger = winston.createLogger({
     transports: [
@@ -55,7 +39,6 @@ const logger = winston.createLogger({
         }),
     ],
 });
-
 
 const connectionClassByName = {
     SerialConnection,
@@ -73,12 +56,6 @@ const fsRecorder = new FileSystemRecorder({
     id: 'fs-destination',
     path: config.loggingPath,
 });
-
-
-const textHeaderSetConsolidator = new HeaderSetConsolidator({
-    timeToLive: config.textLoggingTimeToLive,
-});
-
 
 /**
  * This function is called once the header set is considered "settled".
@@ -134,7 +111,6 @@ const connectToVBus = async () => {
         }
 
         headerSetConsolidator.addHeader(packet);
-        textHeaderSetConsolidator.addHeader(packet);
     });
 
     logger.debug('Connecting to VBus...');
@@ -144,263 +120,11 @@ const connectToVBus = async () => {
     logger.debug('Connected to VBus...');
 };
 
-
-/**
- * Responds to '/cgi-bin/get_resol_device_information' requests.
- * It replies with a fake DL2v2 response to trick VBusTouch to connect with it.
- */
-const processGetResolDeviceInformationRequest = function(req, res) {
-    const content = [
-        'vendor = "RESOL"',
-        'product = "DL2"',
-        'serial = "001E66000000"',
-        'version = "2.1.0"',
-        'build = "201311280853"',
-        'name = "DL2-001E66000000"',
-        'features = "vbus,dl2"',
-    ].join('\n');
-
-    res.status(200);
-    res.set('Content-Type', 'text/plain');
-    res.send(content);
-};
-
-
-/**
- * Allows rewriting a HeaderSet read from the file system before it is send over the network.
- *
- * The example below converts the information from a DeltaSol MX to a Vitosolic 200.
- *
- * @param headerSet A `HeaderSet` instance containing the received `Packet` instances.
- * @returns A `HeaderSet` instance containing the forged `Packet` instances.
- */
-const rewriteWebHeaderSet = function(headerSet) {
-    const timestamp = headerSet.timestamp;
-
-    /*
-     * Get list of packet fields in the received packets.
-     */
-    let packetFields = specification.getPacketFieldsForHeaders(headerSet.getHeaders());
-
-    /*
-     * Map all existing packet fields into an object for easier access.
-     */
-    const origRawValueById = packetFields.reduce((memo, packetField) => {
-        memo [packetField.id] = packetField.rawValue;
-        return memo;
-    }, {});
-
-    /*
-     * Pick the interesting fields from the raw values.
-     */
-    const tempCollector = origRawValueById ['00_0010_7E11_10_0100_000_2_0'];
-    const tempStoreBottom = origRawValueById ['00_0010_7E11_10_0100_002_2_0'];
-    const tempStoreTop = origRawValueById ['00_0010_7E11_10_0100_004_2_0'];
-    const pumpSpeed = origRawValueById ['00_0010_7E11_10_0100_076_1_0'];
-    const info1 = origRawValueById ['00_0010_7E11_10_0100_077_1_0'];
-    const info2 = origRawValueById ['00_0010_7E11_10_0100_078_1_0'];
-    const errorMask = origRawValueById ['00_0010_7E11_10_0100_096_4_0'];
-    const tempFlow = origRawValueById ['00_0010_7E11_10_0100_006_2_0'];
-    const tempReturn = origRawValueById ['00_0010_7E11_10_0100_008_2_0'];
-    const flow = origRawValueById ['00_0010_7E11_10_0100_040_4_0'];
-    const heat = origRawValueById ['00_0010_7E31_10_0100_000_4_0'];
-
-    /*
-     * Create an empty packet of the supported Vitosolic 200.
-     */
-    const supportedPacket1 = new Packet({
-        timestamp,
-        channel: 0,
-        destinationAddress: 0x0010,  // DFA
-        sourceAddress: 0x7321,  // Vitosolic 200 [Regler]
-        command: 0x0100,
-        frameCount: 20,
-    });
-
-    /*
-     * Create an empty packet for the heat quantity information.
-     */
-    const supportedPacket2 = new Packet({
-        timestamp,
-        channel: 0,
-        destinationAddress: 0x0010,  // DFA
-        sourceAddress: 0x7326,  // Vitosolic 200 [WMZ1]
-        command: 0x0100,
-        frameCount: 3,
-    });
-
-    /*
-     * Create a new header set and add the forged packets to it.
-     */
-    headerSet = new HeaderSet({
-        timestamp,
-    });
-    headerSet.addHeader(supportedPacket1);
-    headerSet.addHeader(supportedPacket2);
-
-    /*
-     * Get list of packet fields in the forged packets.
-     */
-    packetFields = specification.getPacketFieldsForHeaders(headerSet.getHeaders());
-
-    /*
-     * Transfered the information picked from the received packets into the forged ones.
-     */
-    specification.setPacketFieldRawValues(packetFields, {
-        '00_0010_7321_10_0100_000_2_0': tempCollector,
-        '00_0010_7321_10_0100_002_2_0': tempStoreBottom,
-        '00_0010_7321_10_0100_004_2_0': tempStoreTop,
-        '00_0010_7321_10_0100_044_1_0': pumpSpeed,
-        '00_0010_7321_10_0100_045_1_0': info1,
-        '00_0010_7321_10_0100_046_1_0': info2,
-        '00_0010_7321_10_0100_060_2_0': errorMask,
-        '00_0010_7326_10_0100_000_2_0': tempFlow,
-        '00_0010_7326_10_0100_002_2_0': tempReturn,
-        '00_0010_7326_10_0100_004_2_0': flow,
-    });
-
-    supportedPacket2.frameData.writeUInt16LE(heat % 1000, 6);
-    supportedPacket2.frameData.writeUInt16LE((heat / 1000) % 1000, 8);
-    supportedPacket2.frameData.writeUInt16LE((heat / 1000000) % 1000, 10);
-
-    /*
-     * Return the forged header set.
-     */
-    return headerSet;
-};
-
-
-/**
- * Responds to '/dlx/download/download' requests.
- * It only implements the parts of the endpoint that are necessary for VBusTouch to work.
- */
-const processDownloadDownloadRequest = async (req, res) => {
-    try {
-        const i18n = specification.i18n;
-
-        const startDate = req.query.startDate ? i18n.momentUtc(req.query.startDate, 'MM/DD/YYYY') : i18n.moment();
-        const endDate = req.query.endDate ? i18n.momentUtc(req.query.endDate, 'MM/DD/YYYY') : i18n.moment();
-
-        const query = {
-            source: req.query.source,
-            outputType: req.query.outputType,
-            dataLanguage: req.query.dataLanguage || 'en',
-            startDate: startDate.toDate(),
-            endDate: endDate.toDate(),
-            sieveInterval: req.query.sieveInterval | 0,
-            ttl: req.query.ttl | 0,
-        };
-
-        // logger.debug(req.query, query);
-
-        const converter1 = new Converter({ objectMode: true });
-
-        const hsc = new HeaderSetConsolidator({
-            timestamp: query.startDate,
-            interval: query.sieveInterval * 1000,
-            timeToLive: query.ttl * 1000,
-        });
-
-        let converter2, contentType;
-        if (query.outputType === 'vbus') {
-            converter2 = new VBusRecordingConverter({});
-            contentType = 'application/octet-stream';
-        } else if (query.outputType === 'json') {
-            converter2 = new DLxJsonConverter({});
-            contentType = 'application/json; charset=utf-8';
-        } else {
-            throw new Error('Unsupported output type ' + JSON.stringify(query.outputType));
-        }
-
-        const onC1HeaderSet = function(headerSet) {
-            // logger.debug('C1 headerSet event received', headerSet.timestamp);
-
-            hsc.processHeaderSet(headerSet);
-        };
-
-        const onC1Finish = function() {
-            // logger.debug('C1 finish event received');
-        };
-
-        converter1.on('headerSet', onC1HeaderSet);
-
-        converter1.on('finish', onC1Finish);
-
-        const onHscHeaderSet = function(headerSet) {
-            if (config.rewriteWebHeaderSets) {
-                headerSet = rewriteWebHeaderSet(headerSet);
-            }
-
-            converter2.convertHeaderSet(headerSet);
-        };
-
-        hsc.on('headerSet', onHscHeaderSet);
-
-        const chunks = [];
-
-        const onC2Data = function(chunk) {
-            // logger.debug('C2 data event received');
-
-            chunks.push(chunk);
-        };
-
-        const onC2End = function() {
-            // logger.debug('C2 end event received');
-        };
-
-        converter2.on('data', onC2Data);
-
-        converter2.on('end', onC2End);
-
-        try {
-            if (query.source === 'current') {
-                converter1.write(headerSetConsolidator);
-            } else {
-                const playerOptions = {
-                    id: 'fs-destination',
-                    interval: config.loggingInterval,
-                    path: config.loggingPath,
-                };
-
-                const playbackOptions = {
-                    minTimestamp: query.startDate,
-                    maxTimestamp: query.endDate,
-                };
-
-                const player = new FileSystemRecorder(playerOptions);
-
-                await player.playback(converter1, playbackOptions);
-            }
-
-            await converter2.finish();
-        } finally {
-            converter1.removeListener('headerSet', onC1HeaderSet);
-            converter1.removeListener('finish', onC1Finish);
-            hsc.removeListener('headerSet', onHscHeaderSet);
-            converter2.removeListener('data', onC2Data);
-            converter2.removeListener('end', onC2End);
-        }
-
-        const buffer = Buffer.concat(chunks);
-
-        res.status(200);
-        res.set('content-type', contentType);
-        res.send(buffer);
-    } catch (err) {
-        global.console.log(err, err.toString(), err.stack);
-
-        res.status(500);
-        res.set('content-type', 'text/plain');
-        res.send(err.toString());
-    }
-};
-
 const startHeaderSetConsolidatorTimer = async () => {
     logger.debug('Starting HeaderSetConsolidator timer...');
 
     headerSetConsolidator.startTimer();
 };
-
 
 const startMqttLogging = async () => {
     const onHeaderSet = async (headerSet, client) => {
@@ -495,56 +219,6 @@ const startMqttLogging = async () => {
     }
 };
 
-const startTextLogging = async () => {
-    let currentDatecode = null;
-
-    let currentConverter = null;
-
-    const onHeaderSet = async (headerSet) => {
-        const datecode = specification.i18n.moment(headerSet.timestamp).format('YYYYMMDD');
-        if (currentDatecode !== datecode) {
-            currentDatecode = datecode;
-
-            if (currentConverter) {
-                currentConverter.finish();
-                currentConverter = null;
-            }
-
-            const filename = path.resolve(config.textLoggingPath, datecode + '.csv');
-
-            const file = fs.createWriteStream(filename, { flags: 'a' });
-
-            const options = Object.assign({}, config.textLoggingOptions, {
-                specification,
-            });
-
-            const converter = new TextConverter(options);
-            converter.pipe(file);
-
-            currentConverter = converter;
-        }
-
-        if (currentConverter) {
-            currentConverter.convertHeaderSet(headerSet);
-        }
-    };
-
-    if (config.textLoggingInterval) {
-        logger.debug('Starting text logging');
-
-        const hsc = new HeaderSetConsolidator({
-            interval: config.textLoggingInterval,
-        });
-
-        hsc.on('headerSet', () => {
-            onHeaderSet(textHeaderSetConsolidator);
-        });
-
-        hsc.startTimer();
-    }
-};
-
-
 const startRecorder = async () => {
     const converter = new Converter({ objectMode: true });
 
@@ -573,8 +247,6 @@ const main = async () => {
     await startHeaderSetConsolidatorTimer();
 
     await startMqttLogging();
-
-    await startTextLogging();
 
     await startRecorder();
 };
